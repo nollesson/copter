97072169a581c22fb6409531fd5e2ab88c712da1
Niklas Ollesson <niklas@ollesson.se>
2018-11-18 00:31:58 +0100

    rename build script

--- buildAndFlashMain.sh
+++ buildAndFlashMain.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+bazel build -c opt --config=arduino :main
 mkdir -p stage
 avr-objcopy -O ihex -R .eeprom bazel-bin/main stage/output.hex
 avrdude -C toolchains/avrdude.conf -v -patmega328p -carduino -P/dev/ttyACM1 -b115200 -Uflash:w:stage/output.hex:i
