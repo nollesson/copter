b6ec358cb4ffa42d9a258e66d1f9d29147d58c64
Niklas Ollesson <niklas@ollesson.se>
2020-01-04 22:12:41 +0100

    I2C 'stable' but get only 0 from sensor

--- Hal/Twi.cpp
+++ Hal/Twi.cpp
@@ -12,131 +12,146 @@ extern "C" {
 #define TWI_WRITE 0
 
 namespace {
-volatile bool _isFinished;
-uint8_t _address;
-uint8_t const *_writeData;
-uint8_t *_readData;
-uint8_t _size;
-uint8_t _status;
-bool _shouldStop;
+    volatile bool _isFinished;
+    uint8_t _address;
+    uint8_t const *_writeData;
+    uint8_t *_readData;
+    uint8_t _size;
+    uint8_t _status;
+    bool _shouldStop;
 
-void start()
-{
-  _isFinished = false;
-  TWCR = (TWCR | (1 << TWINT) | (1 << TWSTA) | (1 << TWIE)) & ~((1 << TWSTO));
-}
+    void start() {
+        _isFinished = false;
+        TWCR = (TWCR | (1 << TWINT) | (1 << TWSTA) | (1 << TWIE)) & ~((1 << TWSTO));
+    }
 
-void stop()
-{
-  _isFinished = true;
-  if (_shouldStop)
-  {
-    TWCR = (TWCR | (1 << TWINT) | (1 << TWSTO)) & ~((1 << TWSTA) | (1 << TWIE));
-  } else {
-    TWCR &= ~(1 << TWIE);
-  }
-}
+    void stop() {
+        _isFinished = true;
+        if (_shouldStop) {
+            TWCR = (TWCR | (1 << TWINT) | (1 << TWSTO)) & ~((1 << TWSTA) | (1 << TWIE));
+        } else {
+            TWCR &= ~(1 << TWIE);
+            _delay_ms(1); // Doesn't behave stable without this, don't know why
+        }
+    }
 } // namespace
 
 namespace twi {
 /*
  * Sets up TWI. Needs to be called before any other functions.
  */
-void init() {
-  // Set frequency to TWI_FREQUENCY
-  TWSR &= ~((1 << TWPS0) | (1 << TWPS1));
-  TWBR = ((F_CPU / TWI_FREQUENCY) - 16) / 2;
-  TWCR = (1 << TWEN) | (1 << TWIE);
-}
+    void init() {
+        // Set frequency to TWI_FREQUENCY
+        TWSR &= ~((1 << TWPS0) | (1 << TWPS1));
+        TWBR = ((F_CPU / TWI_FREQUENCY) - 16) / 2;
+        TWCR = (1 << TWEN) | (1 << TWIE);
+    }
 
 /*
  * Tests if there is a response at given address
  * \param address 7-bit address for TWI slave
  * \return true if slave responds to address
  */
-bool testAddress(uint8_t const address) {
-  _size = 0;
-  _shouldStop = true;
-  _address = (address << 1) + TWI_WRITE;
+    bool testAddress(uint8_t const address) {
+        _size = 0;
+        _shouldStop = true;
+        _address = (address << 1) + TWI_WRITE;
 
-  start();
-  while (!_isFinished);
-  return _status != TW_MT_SLA_NACK;
-}
-
-bool writeData(uint8_t const address, uint8_t const *const data, int const size, bool shouldStop) {
-  _size = size;
-  _writeData = data;
-  _shouldStop = shouldStop;
-  _address = (address << 1) + TWI_WRITE;
-
-  start();
-  while (!_isFinished);
-  return _status == TW_MT_DATA_ACK;
-}
-
-bool readData(uint8_t const address, uint8_t *const data, int const size, bool shouldStop) {
-  _isFinished = false;
-  _size = size;
-  _readData = data;
-  _shouldStop = shouldStop;
-  _address = (address << 1) + TWI_READ;
-
-  start();
-  while (!_isFinished);
-  return _status == TW_MR_DATA_ACK;
-}
-
-ISR(TWI_vect) {
-  _status = TW_STATUS;
-
-  switch (_status) {
-  case TW_START:
-  case TW_REP_START:
-    TWDR = _address;
-    TWCR |= 1 << TWINT;
-    break;
-  case TW_MT_SLA_ACK:
-  case TW_MT_DATA_ACK:
-    if (_size > 0) {
-      TWDR = *_writeData;
-      TWCR = (TWCR | (1 << TWINT)) & ~((1 << TWSTA) | (1 << TWSTO));
-      ++_writeData;
-      --_size;
-    } else {
-      stop();
+        start();
+        while (!_isFinished);
+        return _status != TW_MT_SLA_NACK;
     }
-    break;
-  case TW_MT_SLA_NACK:
-  case TW_MT_DATA_NACK:
-    stop();
-    break;
-  case TW_MR_SLA_ACK:
-    TWCR = (TWCR | ((1 << TWINT) | (1 << TWEA))) & ~((1 << TWSTA) | (1 << TWSTO));
-    break;
-  case TW_MR_DATA_ACK:
-    *_readData = TWDR;
-    if (_size > 1)
-    {
-      TWCR = (TWCR | ((1 << TWINT) | (1 << TWEA))) & ~((1 << TWSTA) | (1 << TWSTO));
-    } else {
-      TWCR = (TWCR | ((1 << TWINT))) & ~((1 << TWSTA) | (1 << TWSTO) | (1 << TWEA));
+
+    bool writeData(uint8_t const address, uint8_t const *const data, int const size, bool shouldStop) {
+        _size = size;
+        _writeData = data;
+        _shouldStop = shouldStop;
+        _address = (address << 1) + TWI_WRITE;
+
+        start();
+        while (!_isFinished);
+        return _status == TW_MT_DATA_ACK;
     }
-    --_size;
-    ++_readData;
-    break;
-  case TW_MR_SLA_NACK:
-  case TW_MR_DATA_NACK:
-    stop();
-    break;
-  case TW_MT_ARB_LOST: // Same value as TW_MR_ARB_LOST
-    _isFinished = true;
-    TWCR |= 1 << TWINT;
-    break;
-  default:
-    Serial.print("Unhandled case: 0x");
-    Serial.println(_status, HEX);
-    break;
-  }
-}
+
+    bool readData(uint8_t const address, uint8_t *const data, int const size, bool shouldStop) {
+        _isFinished = false;
+        _size = size;
+        _readData = data;
+        _shouldStop = shouldStop;
+        _address = (address << 1) + TWI_READ;
+
+        start();
+        while (!_isFinished);
+        return _status == TW_MR_DATA_ACK;
+    }
+
+    ISR(TWI_vect) {
+            _status = TW_STATUS;
+
+            switch (_status) {
+                case TW_START:
+                case TW_REP_START:
+                    Serial.print("START");
+                    Serial.print("--");
+                    Serial.println(TW_STATUS, HEX);
+                    TWDR = _address;
+                    TWCR |= 1 << TWINT;
+                    break;
+                case TW_MT_SLA_ACK:
+                case TW_MT_DATA_ACK:
+                    if (_size > 0) {
+                        Serial.print("SEND--");
+                        Serial.print(*_writeData, HEX);
+                        Serial.print("--");
+                        Serial.println(TW_STATUS, HEX);
+                        TWDR = *_writeData;
+                        TWCR = (TWCR | (1 << TWINT)) & ~((1 << TWSTA) | (1 << TWSTO));
+                        ++_writeData;
+                        --_size;
+                    } else {
+                        Serial.print("SEND DONE");
+                        Serial.print("--");
+                        Serial.println(TW_STATUS, HEX);
+                        stop();
+                    }
+                    break;
+                case TW_MT_SLA_NACK:
+                case TW_MT_DATA_NACK:
+                    Serial.println("STOP FROM SLAVE");
+                    stop();
+                    break;
+                case TW_MR_SLA_ACK:
+                    Serial.println("SLACK");
+                    TWCR = (TWCR | ((1 << TWINT) | (1 << TWEA))) & ~((1 << TWSTA) | (1 << TWSTO));
+                    break;
+                case TW_MR_DATA_ACK:
+                    *_readData = TWDR;
+                    Serial.print("READ--");
+                    Serial.print(*_readData, HEX);
+                    Serial.print("--");
+                    Serial.println(TW_STATUS, HEX);
+                    if (_size > 1) {
+                        TWCR = (TWCR | ((1 << TWINT) | (1 << TWEA))) & ~((1 << TWSTA) | (1 << TWSTO));
+                    } else {
+                        TWCR = (TWCR | ((1 << TWINT))) & ~((1 << TWSTA) | (1 << TWSTO) | (1 << TWEA));
+                    }
+                    --_size;
+                    ++_readData;
+                    break;
+                case TW_MR_SLA_NACK:
+                case TW_MR_DATA_NACK:
+                    Serial.println("SLNACK");
+                    stop();
+                    break;
+                case TW_MT_ARB_LOST: // Same value as TW_MR_ARB_LOST
+                    Serial.print("ARBLOST");
+                    _isFinished = true;
+                    TWCR |= 1 << TWINT;
+                break;
+                default:
+                    Serial.print("Unhandled case: 0x");
+                    Serial.println(_status, HEX);
+                    break;
+            }
+    }
 } // namespace twi
--- StreamSensor.cpp
+++ StreamSensor.cpp
@@ -3,33 +3,40 @@
 
 #include <util/delay.h>
 
+extern "C" {
+#include <avr/io.h>
+#include <compat/twi.h>
+}
+
 namespace {
-uint8_t IMU_ADDRESS = 0x68;
-uint8_t REG_ACCEL_DATA = 0x3B;
-uint8_t REG_GYRO_DATA = 0x43;
+    uint8_t IMU_ADDRESS = 0x68;
+    uint8_t REG_ACCEL_DATA = 0x3B;
+    uint8_t REG_GYRO_DATA = 0x43;
 }
 
 int main() {
-  (void)REG_ACCEL_DATA;
-  sei();
-  Serial.begin(9600);
-  twi::init();
+    (void) REG_GYRO_DATA;
+    (void) REG_ACCEL_DATA;
+    sei();
+    Serial.begin(9600);
+    twi::init();
 
-  while (1) {
-    twi::writeData(IMU_ADDRESS, &REG_GYRO_DATA, 1, false);
-    uint8_t gyroData[6];
-    twi::readData(IMU_ADDRESS, gyroData, 6, true);
+    while (1) {
+        uint8_t a = 0x75;
+        twi::writeData(IMU_ADDRESS, &a, 1, false);
+        uint8_t gyroData[6];
+        twi::readData(IMU_ADDRESS, gyroData, 1, true);
 
-    int16_t x = static_cast<int16_t>(gyroData[0]);
-    int16_t y = static_cast<int16_t>(gyroData[2]);
-    int16_t z = static_cast<int16_t>(gyroData[4]);
+        int16_t x = static_cast<int16_t>(gyroData[0]);
+        int16_t y = static_cast<int16_t>(gyroData[2]);
+        int16_t z = static_cast<int16_t>(gyroData[4]);
 
-    Serial.print(x);
-    Serial.print(", ");
-    Serial.print(y);
-    Serial.print(", ");
-    Serial.println(z);
+        Serial.print(x);
+        Serial.print(", ");
+        Serial.print(y);
+        Serial.print(", ");
+        Serial.println(z);
 
-    _delay_ms(50000);
-  }
+        _delay_ms(50000);
+    }
 }
